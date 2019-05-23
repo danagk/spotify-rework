9bcc5e3b2a5614f211e7903aa4d959b7b1932278
JuliaKoh <jkoh16@uw.edu>
2019-05-23 13:21:02 -0700

    Fix lintr errors and add comments

--- main_data_frame.R
+++ main_data_frame.R
@@ -1,24 +1,35 @@
 library("spotifyr")
 library("tidyverse")
-library("knitr")
 library("dplyr")
-source(api_key.R)
+source("api_keys.R")
 
+# Creation of data frame from Spotify API --------------------------------------
+
+# For each the genre following steps apply:
+# (1) Use `get_recommendations()` to obtain 100 songs of the genre and store in
+# `"genre"_songs` data frame. 
+# (2) Store the song features in another using `get_track_audio_feature()` by 
+# the id in the `"genre"_songs`` data frame. 
+# (3) Merge the two dataframes together into the `"genre"_songs` data frame by 
+# `id`
+
+# Jazz songs
 jazz_songs <- get_recommendations(limit = 100,
-                                  seed_genres = "jazz") %>% 
-                                  mutate(genre = "jazz") %>% 
+                                  seed_genres = "jazz") %>%
+                                  mutate(genre = "jazz") %>%
                                   select(genre, name, id)
-jazz_song_features = get_track_audio_features(jazz_songs$id)
+jazz_song_features <-  get_track_audio_features(jazz_songs$id)
 jazz_songs <- merge(jazz_songs, jazz_song_features, by = "id")
 
+# Rock Songs
 rock_songs <- get_recommendations(limit = 100,
-                                  seed_genres = "rock") %>% 
-                                  mutate(genre = "rock") %>% 
-                                  select(genre, name, id) 
-
-rock_songs_features = get_track_audio_features(rock_songs$id)
+                                  seed_genres = "rock") %>%
+                                  mutate(genre = "rock") %>%
+                                  select(genre, name, id)
+rock_songs_features <- get_track_audio_features(rock_songs$id)
 rock_songs <- merge(rock_songs, rock_songs_features, by = "id")
 
+# Pop Songs
 pop_songs <- get_recommendations(limit = 100,
                                 seed_genres = "pop") %>%
                                 mutate(genre = "pop") %>%
@@ -26,13 +37,15 @@ pop_songs <- get_recommendations(limit = 100,
 pop_songs_features <- get_track_audio_features(pop_songs$id)
 pop_songs <- merge(pop_songs, pop_songs_features, by = "id")
 
+# Hip Hop Songs
 hip_hop_songs <- get_recommendations(limit = 100,
-                                      seed_genres = "hip-hop") %>% 
-                                      mutate(genre = "hip-hop") %>% 
-                                      select(genre, name, id) 
+                                      seed_genres = "hip-hop") %>%
+                                      mutate(genre = "hip-hop") %>%
+                                      select(genre, name, id)
 hip_hop_songs_features <- get_track_audio_features(hip_hop_songs$id)
 hip_hop_songs <- merge(hip_hop_songs, hip_hop_songs_features, by = "id")
 
+# Country Songs
 country_songs <- get_recommendations(limit = 100,
                                      seed_genres = "country") %>%
                                      mutate(genre = "country") %>%
@@ -40,13 +53,15 @@ country_songs <- get_recommendations(limit = 100,
 country_songs_features <- get_track_audio_features(country_songs$id)
 country_songs <- merge(country_songs, country_songs_features, by = "id")
 
+# Classical Songs
 classical_songs <- get_recommendations(limit = 100,
                                      seed_genres = "classical") %>%
                                      mutate(genre = "classical") %>%
-                                     select(genre, name, id) 
+                                     select(genre, name, id)
 classical_songs_features <- get_track_audio_features(classical_songs$id)
 classical_songs <- merge(classical_songs, classical_songs_features, by = "id")
 
+# Metal Songs
 metal_songs <- get_recommendations(limit = 100,
                                        seed_genres = "metal") %>%
                                        mutate(genre = "metal") %>%
@@ -54,8 +69,9 @@ metal_songs <- get_recommendations(limit = 100,
 
 metal_songs_features <- get_track_audio_features(metal_songs$id)
 metal_songs <- merge(metal_songs, metal_songs_features, by = "id")
-                                       
 
+# Final Data frame for use -----------------------------------------------------                                     
+
+# Combine all the "genre"_songs data frames using `rbind`
 songs_df <- rbind(jazz_songs, rock_songs, pop_songs, hip_hop_songs,
                   country_songs, classical_songs, metal_songs)
-
