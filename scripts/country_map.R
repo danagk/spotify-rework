1c8297b6e4d3466ff6a3e7c3f9acac29d856dab4
danagk <d.korssjoen@outlook.com>
2019-05-24 14:18:09 -0700

    Add interactive map

--- scripts/country_map.R
+++ scripts/country_map.R
@@ -2,12 +2,14 @@
 # country of North America
 
 library("dplyr")
-library("leaflet")
-library("spData")
+library("plotly")
+library("ggplot2")
 
 # read csv data with track info by country
-country_data <- read.csv(file = "../data/country_data.csv", stringsAsFactors 
-                         = F)
+country_data <- read.csv(
+  file = "../data/country_data.csv", stringsAsFactors
+  = F
+)
 
 # group data by selected audio characteristic
 # in final version of report, this will be interactive
@@ -16,18 +18,31 @@ by_country <- country_data %>%
   group_by(country = playlist_name) %>%
   summarise(valence = mean(valence))
 
+# set country names to match for merge
+by_country$country[by_country$country == "United States"] <- "USA"
+
 # join this df to shapefile
-merged_countries <- merge(by_country, world, by.x = "country", by.y = 
-                            "name_long")
+country_shape <- map_data("world", region = by_country$country) %>%
+  rename(country = region) %>%
+  merge(by_country, by = "country")
 
 # create map shaded according to selected characteristic (valence)
-countries_map <- leaflet(world) %>%
-  addProviderTiles(providers$CartoDB.Positron) %>%
-  setView(-100, 39, zoom = 2) %>%
-  addPolygons()
+country_map <- ggplot(country_shape) +
+  geom_polygon(
+    mapping = aes(x = long, y = lat, group = group, fill = valence),
+    color = "white",
+    size = .1
+  ) + coord_map(xlim = c(-175, -45), ylim = c(-10, 80))
 
-# still to do:
-#   edit map styles
-#   create palette
-#   shade map according to values in by_country
-#   check styler and lintr for errors
+# make interactive with plotly
+test <- ggplotly(country_map) %>%
+  layout(
+    title = "Average Valence in Top 50 Songs",
+    xaxis = list(title = ""),
+    yaxis = list(title = ""),
+    geo = list(
+      scope = "north america",
+      lonaxis = list(range = c(-175, -45)),
+      lataxis = list(range = c(-10, 80))
+    )
+  )
