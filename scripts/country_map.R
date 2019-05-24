e9065964c521de646de2214b4d1e0653a17df033
danagk <d.korssjoen@outlook.com>
2019-05-23 23:59:03 -0700

    Create base map

--- scripts/country_map.R
+++ scripts/country_map.R
@@ -2,9 +2,12 @@
 # country of North America
 
 library("dplyr")
+library("leaflet")
+library("spData")
 
 # read csv data with track info by country
-country_data <- read.csv(file = "../data/country_data.csv", stringsAsFactors = F)
+country_data <- read.csv(file = "../data/country_data.csv", stringsAsFactors 
+                         = F)
 
 # group data by selected audio characteristic
 # in final version of report, this will be interactive
@@ -13,5 +16,18 @@ by_country <- country_data %>%
   group_by(country = playlist_name) %>%
   summarise(valence = mean(valence))
 
-# create map shaded according to desired characteristic
-# code goes here
+# join this df to shapefile
+merged_countries <- merge(by_country, world, by.x = "country", by.y = 
+                            "name_long")
+
+# create map shaded according to selected characteristic (valence)
+countries_map <- leaflet(world) %>%
+  addProviderTiles(providers$CartoDB.Positron) %>%
+  setView(-100, 39, zoom = 2) %>%
+  addPolygons()
+
+# still to do:
+#   edit map styles
+#   create palette
+#   shade map according to values in by_country
+#   check styler and lintr for errors
