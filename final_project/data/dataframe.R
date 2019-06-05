5799333f2e916369bb5a3f4ddf59e9b0268ea82d
danagk <d.korssjoen@outlook.com>
2019-06-05 09:14:49 -0700

    Move data directory

---
+++ final_project/data/dataframe.R
@@ -0,0 +1,5 @@
+songs_df <- read.csv("./data/SpotifyFeatures.csv", stringsAsFactors = FALSE) %>%
+  # Filter genres that either do not have enough observations or we deemed
+  # as irrelevant genres
+  filter(genre != "A Capella" & genre != "Comedy" & 
+           genre != "Movie" & genre != "Soundtrack")
