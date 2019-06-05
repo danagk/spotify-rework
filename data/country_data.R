8bf4789b662f553de6ed076f016ffb5e2718695b
danagk <d.korssjoen@outlook.com>
2019-06-05 09:21:15 -0700

    Move data files

--- data/country_data.R
+++
@@ -1,4 +0,0 @@
-# this file converts the csv dataset into an R dataframe
-
-# The Top 50 by Country Dataset
-country_data <- read.csv(file = "../data/country_data.csv", stringsAsFactors = F)
--- data/dataframe.R
+++
@@ -1,5 +0,0 @@
-songs_df <- read.csv("../data/SpotifyFeatures.csv", stringsAsFactors = FALSE) %>%
-  # Filter genres that either do not have enough observations or we deemed
-  # as irrelevant genres
-  filter(genre != "A Capella" & genre != "Comedy" & 
-           genre != "Movie" & genre != "Soundtrack")
